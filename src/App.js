294e14d4bcca09ea7ee553fdc642135cd387915d
JohiChawala <136928760+JohiChawala@users.noreply.github.com>
2023-11-21 02:37:44 +0500

    offering.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/NavbarHead.js';
 import Welcome from './components/Welcome.js';
+import Offering from './components/Offering.js';
 
 function App() {
   return (
     <>
       <Navbar/>
       <Welcome/>
-
+      <Offering/>
     </>
     
   );
